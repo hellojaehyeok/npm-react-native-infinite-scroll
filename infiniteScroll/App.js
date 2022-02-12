3ab4de69262e95f5a3103ac15b120f88f90d36b4
Song jae hyeok <wakij6587@naver.com>
2022-02-12 22:54:37 +0900

    react native init

---
+++ infiniteScroll/App.js
@@ -0,0 +1,18 @@
+
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+const App = () => {
+
+
+  return (
+    <View>
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
